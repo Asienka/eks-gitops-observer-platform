6499deb2083e42df0515c6854521b97055774b81
Joanna <ulanowska.joanna@gmail.com>
2026-06-14 14:14:46 +0200

    add favicon endpoint to FastAPI application to avoid 404 error in pod logs

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI 
+from fastapi import FastAPI, Response, status
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(
@@ -23,3 +23,7 @@ APP_VERSION = os.getenv("APP_VERSION", "1.0.0-local")
 @app.get("/version")
 def get_version():
     return {"version": APP_VERSION}
+
+@app.get("/favicon.ico", include_in_schema=False)
+def get_favicon():
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
